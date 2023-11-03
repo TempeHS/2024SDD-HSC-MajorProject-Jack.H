538d12c04954d6b1606719393ca16891fa1ce2d2
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-11-03 11:05:19 +1100

    Pseudocode for record to json

--- myPWA/index.js
+++ myPWA/index.js
@@ -3,6 +3,28 @@ const db = new sqlite3.Database('.database/datasource.db');
 
 let myString = '123';
 
+/*
+START
+GET sddstudents
+DISPLAY writeRecord(sddstudents)
+END
+
+START SUBROUTINE writeRecord(record)
+    LET myString = '[\n'
+    LET counter = 0
+    FOR EACH item IN record
+        LET myString = myString + '{\n"nesaID":' + record.nesaID[counter] + ",\n" + '{\n"name":' + record.name[counter] + ",\n" + '{\n"age":' + record.nesaID[age] + "\n}"
+        IF counter + 1 = record.length
+            LET myString = myString + "\n]"
+        ELSE IF 
+            LET myString = myString + ",\n"
+        END IF
+        LET counter = counter + 1
+    END FOR EACH
+    RETURN myString
+END SUBROUTINE
+*/
+
 
 var fs = require('fs');
 fs.writeFile("public/frontEndData.json", myString + "]", function(err) {
