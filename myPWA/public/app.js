9e223dc294b6ff5b3c0dedf2804f1b8e5b9d47e8
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2024-02-02 17:43:06 +1100

    Test for device orientation on ios
    request function taken from https://forums.developer.apple.com/forums/thread/128376

--- myPWA/public/app.js
+++ myPWA/public/app.js
@@ -307,10 +307,26 @@
   }
 
   ondeviceorientation = (event) => {
+    requestDeviceOrientation();
     if (gameOn && defaultBeta != undefined) {
-      //document.getElementById("scoreDisplay").innerHTML = "Orientation: " + event.beta;
+      document.getElementById("scoreDisplay").innerHTML = "Orientation: " + event.beta;
     } else {
       defaultBeta = event.beta;
       defaultGamma = event.gamma;
     }
   };
+
+  function requestDeviceOrientation () {
+    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission()
+    .then(permissionState => {
+    if (permissionState === 'granted') {
+    window.addEventListener('deviceorientation', () => {});
+    }
+    })
+    .catch(console.error);
+    } else {
+    // handle regular non iOS 13+ devices
+    console.log ("not iOS");
+    }
+  }
