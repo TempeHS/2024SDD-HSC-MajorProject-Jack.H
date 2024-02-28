574d8604188a0c1c1721fea6704884b43d6f7a9e
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2024-02-28 22:40:29 +1100

    Added slight failsafes to generation

--- myPWA/public/gameplay.js
+++ myPWA/public/gameplay.js
@@ -326,9 +326,17 @@ function pauseMenu() {
       if (diamond[i].split(",")[0] < 80 + size * 3.15 && diamond[i].split(",")[1] < 100 + size * 3.15) {
         diamond.splice(i, 1);
         i--;
-      } else if (diamond[i].split(",")[0] > width - size * 3.4 && diamond[i].split(",")[1] > height - size * 3.4) {
+      } else if (diamond[i].split(",")[0] > width - size * 4 && diamond[i].split(",")[1] > height - size * 4) {
         diamond.splice(i, 1);
         i--;
+      }
+      if (diamond[i].split(",")[0] > width - size * 6 || diamond[i].split(",")[1] > height - size * 6) {
+        for (let j=0; j<diamond.length-1; j++) {
+          if (diamond[j].split(",")[0] > width - size * 6 || diamond[j].split(",")[1] > height - size * 6) {
+            diamond[i].splice(i, 1);
+            j = diamond.length-1;
+          }
+        }
       }
     }
 
@@ -346,8 +354,16 @@ function pauseMenu() {
       } else if (wall[i].split(",")[0] > width - size * 5 && wall[i].split(",")[1] > height - size * 5) {
         wall.splice(i, 1);
         i--;
+      } 
+      for (let j=0; j<wall.length-1; j++) {
+        if (wall[i].split(",")[0] - wall[j].split(",")[0] < 5 && wall[i].split(",")[2] == 0 && wall[j].split(",")[2] == 0) {
+          wall.splice(i, 1);
+        } else if (wall[i].split(",")[1] - wall[j].split(",")[1] < 5 && wall[i].split(",")[2] == 90 && wall[j].split(",")[2] == 90) {
+          wall.splice(i, 1);
+        }
       }
     }
+
 
     // Generate mines
     mine = [];
@@ -399,7 +415,7 @@ function pauseMenu() {
     document.getElementById("retry").style.display = "inline";
     document.getElementById("pausePage").style.display = "block";
     music.pause();
-    navigator.vibrate(300);
+    navigator.vibrate(1000);
   }
 
   // Checks for collision between ball and obstacles
